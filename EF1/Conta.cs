39b8dc3682406c1c5d586687e64c985329a797b1
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2023-04-05 22:47:09 -0300

    Adicionar arquivos de projeto.

---
+++ EF1/Conta.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EF1
+{
+    internal class Conta
+    {
+        private int _nConta;
+        private double _saldo;
+        public string NomeTitular;
+        
+        
+        public Conta(string nomeTitular, int nConta)
+        {
+            NomeTitular = nomeTitular;
+            _nConta = nConta;
+            _saldo = 0;
+        }
+
+        public Conta(int nConta, string nomeTitular, double deposito) : this(nomeTitular, nConta)
+        {
+            Deposito(deposito);
+        }
+
+        public double Deposito(double deposito)
+        {
+            return _saldo += deposito;
+        }
+
+        public double Saque(double saque)
+        {
+            double valorComTaxa = saque + 5.0;
+            return _saldo -= valorComTaxa;
+        }
+
+
+        public double NConta
+        {
+            get { return _nConta; }
+        }
+
+        public double Saldo
+        {
+            get { return _saldo; }  
+        }
+
+        public override string ToString()
+        {
+            return "Conta " 
+                + NConta 
+                + ", Titular: " 
+                + NomeTitular 
+                + ", Saldo: $ " 
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ EF1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace EF1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o número da conta: ");
+            int nConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string titularConta = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n) ? ");
+            string simOuNao = Console.ReadLine();
+
+            Conta conta;
+
+            if (simOuNao == "n")
+            {
+                conta = new Conta(titularConta, nConta);
+            }
+            else
+            {
+                Console.WriteLine("Entre o valor do depósito inicial:");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(nConta, titularConta, depositoInicial);
+            }
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre com um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(deposito);
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saque);
+            Console.WriteLine(conta);
+        }
+    }
+}
